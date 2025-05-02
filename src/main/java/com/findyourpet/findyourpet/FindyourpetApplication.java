0a84cec14811dcc3f0fe35cf31985b17a7f2724f
Cristiana Precup <cristianaprecup65@gmail.com>
2025-05-02 16:00:31 +0300

    intial springboot setup

---
+++ src/main/java/com/findyourpet/findyourpet/FindyourpetApplication.java
@@ -0,0 +1,13 @@
+package com.findyourpet.findyourpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyourpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindyourpetApplication.class, args);
+	}
+
+}
