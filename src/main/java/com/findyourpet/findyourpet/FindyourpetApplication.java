ead2a3fb6eaa12ca8e16e45f3220cee6d92714c8
Cristiana Precup <cristianaprecup65@gmail.com>
2025-05-05 23:48:36 +0300

    fix connection

--- src/main/java/com/findyourpet/findyourpet/FindyourpetApplication.java
+++ src/main/java/com/findyourpet/findyourpet/FindyourpetApplication.java
@@ -2,12 +2,19 @@ package com.findyourpet.findyourpet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class FindyourpetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FindyourpetApplication.class, args);
 	}
 
+	@GetMapping("/api/hello")
+	public String hello() {
+		return "TEST: backend connectedf!";
+	}
 }
---
+++ src/main/java/com/findyourpet/findyourpet/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.findyourpet.findyourpet;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+// disable the auth for intial test between frontend and backend
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
