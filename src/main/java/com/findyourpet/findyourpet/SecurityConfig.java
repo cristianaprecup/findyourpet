3aba4d714caa1793bd4a4cdb07a0614ea8e45116
nitugabriela <gabrielanitu34@yahoo.com>
2025-05-20 17:42:54 +0300

    security

--- src/main/java/com/findyourpet/findyourpet/SecurityConfig.java
+++ src/main/java/com/findyourpet/findyourpet/SecurityConfig.java
@@ -1,22 +1,28 @@
-package com.findyourpet.findyourpet;
+package com.findyourpet.findyourpet.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-// disable the auth for intial test between frontend and backend
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
-
-        return http.build();
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
     }
 }
